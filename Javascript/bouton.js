3a3770ecba35e7cb8e05feedfb90f83299ff0cba
T0r4k <t0r4k@hotmail.fr>
2015-05-10 15:55:59 +0200

    c'est bientot la fin

--- Javascript/bouton.js
+++ Javascript/bouton.js
@@ -1,3 +1,12 @@
-function changerStyle(altstyle){
+function changerScreen(altstyle){
 	document.getElementById("SP").setAttribute('href',altstyle);
+}
+
+function changerPrint(altprint){
+	document.getElementById("PP").setAttribute('href',altprint);
+}
+
+function changerStyle(altstyle, altprint){
+	changerScreen(altstyle);
+	changerPrint(altprint);
 }
