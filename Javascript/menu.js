06998339ab286ad192edb9aff6e4e02b2efd345b
T0r4k <t0r4k@hotmail.fr>
2015-05-12 20:39:22 +0200

    aze

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,20 +1,18 @@
 function genereSommaire(){
   var sommaire = document.getElementById('divmenu');
-  var contenu = document.getElementById('contenu');
-  var H2 = contenu.getElementsByTagName('h2');
+  var H2 = document.getElementsByTagName('h2');
   for (var i=0;  i< H2.length; i++) {
-    var unId=H2[i].id="h"+(i+1);
+    var unId=H2[i].id="id"+(i+1);
     var myI =((i+1)<10)?'0'+(i+1):(i+1);
     var hrf= H2[i].childNodes[0];
     var txt = ' ' + hrf.textContent;
     var a = document.createElement('a');
     a.setAttribute('href',"#"+unId);
-    label = document.createTextNode(myI + '.'+ txt );
-    a.appendChild(label);
+    noeud = document.createTextNode(myI + '.'+ txt );
+    a.appendChild(noeud);
     var br = document.createElement('br');
     sommaire.appendChild(a);
-    sommaire.appendChild(br);
-    
+    sommaire.appendChild(br); 
     }
 }
 
@@ -29,5 +27,6 @@ function position(){
   menu.style.width="16%";
   genereSommaire();
   
-  menu.style.border="solid black 1px"
+  menu.style.border="solid black 1px";
+  menu.style.borderRadius="8px";
 }
